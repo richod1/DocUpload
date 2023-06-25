912e4f2b44b7748989e33ce1681c2efaca2508a0
richod <frimpsup@gmail.com>
2023-06-25 01:53:40 -0400

    0doc mv

--- 0docv1.py
+++ 0docv1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os;
-from dotenv import load_dotenv;
+from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
